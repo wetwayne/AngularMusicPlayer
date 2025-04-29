e0e50312509df27e109310ad88c95693d2534fe4
wetwayne <nwestervelt@proton.me>
2025-04-28 20:57:46 -0500

    Resolve 'Cannot Use Import Statement' Error
    Resolve the 'Cannot use import statement outside a module' error and
    remove extraneous comments.

--- music-server/src/main.ts
+++ music-server/src/main.ts
@@ -1,10 +1,5 @@
-// Import the express in typescript file
 import express from 'express';
-
-// Initialize the express engine
 const app: express.Application = express();
-
-// Take a port 3000 for running server.
 const port: number = 3000;
 
 // Handling '/' Request
