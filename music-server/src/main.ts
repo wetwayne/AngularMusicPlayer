d33e29d583c499b375d6ee7ec8aaa2ed6a725f47
wetwayne <nwestervelt@proton.me>
2025-04-29 20:39:40 -0500

    Working test request for song file setup.

--- music-server/src/main.ts
+++ music-server/src/main.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 const app: express.Application = express();
 const port: number = 3000;
+const appRoot = '/wetwayne-music-server'
 
-app.get('/', (req, res) => {
-	res.send("TypeScript With Express");
+
+app.get(`${appRoot}/:sondId`, (req, res) => {
+	console.log('received')
+	res.sendFile('/home/noah/Music/Anamanaguchi/00 - Anamanaguchi - Miku feat. Hatsune Miku.flac')
 });
 
 app.listen(port, () => {
 	console.log(`TypeScript with Express 
-		http://localhost:${port}/`);
+		http://localhost:${port}${appRoot}/`);
 });
 
