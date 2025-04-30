3b15b94679526d6f4a58b30e1d8aecd91f9676bf
wetwayne <nwestervelt@proton.me>
2025-04-29 20:03:00 -0500

    Remove extra comments from server's main.ts file.

--- music-server/src/main.ts
+++ music-server/src/main.ts
@@ -2,12 +2,10 @@ import express from 'express';
 const app: express.Application = express();
 const port: number = 3000;
 
-// Handling '/' Request
-app.get('/', (_req, _res) => {
-	_res.send("TypeScript With Express");
+app.get('/', (req, res) => {
+	res.send("TypeScript With Express");
 });
 
-// Server setup
 app.listen(port, () => {
 	console.log(`TypeScript with Express 
 		http://localhost:${port}/`);
